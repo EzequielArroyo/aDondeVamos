11ae2cc97ea1d65f9dcab6129a4cafe9ca577b15
EZEarroyo <ezarroyo@frba.utn.edu.ar>
2026-03-28 12:05:51 -0300

    refactor: Change controller test endpoint

--- post-service/src/main/java/com/github/ezequielarroyo/postservice/controllers/postController.java
+++ post-service/src/main/java/com/github/ezequielarroyo/postservice/controllers/postController.java
@@ -3,10 +3,10 @@ package com.github.ezequielarroyo.postservice.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController(value = "post")
 public class postController {
-    @GetMapping()
+    @GetMapping(value = "/ping")
     public String helloWorld(){
-        return "hello world";
+        return "Adonde vamos respond: pong";
     }
 }
