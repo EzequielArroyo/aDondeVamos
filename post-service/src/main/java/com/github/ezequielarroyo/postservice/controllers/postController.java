e4a2299e29ec63e788366911799fcd4b7cef4783
EZEarroyo <ezarroyo@frba.utn.edu.ar>
2026-04-15 18:02:28 -0300

    feat: api gateway redirect to post-service

--- post-service/src/main/java/com/github/ezequielarroyo/postservice/controllers/postController.java
+++ post-service/src/main/java/com/github/ezequielarroyo/postservice/controllers/postController.java
@@ -1,9 +1,11 @@
 package com.github.ezequielarroyo.postservice.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value = "post")
+@RestController
+@RequestMapping("/posts")
 public class postController {
     @GetMapping(value = "/ping")
     public String helloWorld(){
