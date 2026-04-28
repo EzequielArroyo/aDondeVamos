525c2995fc20a342718842f94806106eb6f50d18
EZEarroyo <ezarroyo@frba.utn.edu.ar>
2026-04-28 11:30:30 -0300

    refactor: change on name method

--- post-service/src/main/java/com/github/ezequielarroyo/postservice/repositories/IUserRepository.java
+++ post-service/src/main/java/com/github/ezequielarroyo/postservice/repositories/IUserRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 import java.util.UUID;
 @Repository
 public interface IUserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUuid(UUID uuid);
+    Optional<User> findByUserId(UUID uuid);
 }
