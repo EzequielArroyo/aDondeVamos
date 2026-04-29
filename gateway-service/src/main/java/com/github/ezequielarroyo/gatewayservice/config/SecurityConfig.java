a96eae67edb71f672163ad40665689a70ec82c9f
EZEarroyo <ezarroyo@frba.utn.edu.ar>
2026-04-29 10:07:17 -0300

    fix: SpringSecurity config

--- gateway-service/src/main/java/com/github/ezequielarroyo/gatewayservice/config/SecurityConfig.java
+++ gateway-service/src/main/java/com/github/ezequielarroyo/gatewayservice/config/SecurityConfig.java
@@ -3,18 +3,19 @@ package com.github.ezequielarroyo.gatewayservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authorize) -> authorize
-                        .anyRequest().authenticated()
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer((oauth2) -> oauth2
                         .jwt(Customizer.withDefaults())
