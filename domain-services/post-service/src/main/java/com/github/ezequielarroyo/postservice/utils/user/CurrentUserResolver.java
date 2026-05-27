11aa41031c4aee6b4b458edc236c3e374b6ce93b
EZEarroyo <ezarroyo@frba.utn.edu.ar>
2026-05-27 20:29:43 -0300

    feat(CurrentUserResolver): add log for possible error + replace class constructor with lombok annotation.

--- domain-services/post-service/src/main/java/com/github/ezequielarroyo/postservice/utils/user/CurrentUserResolver.java
+++ domain-services/post-service/src/main/java/com/github/ezequielarroyo/postservice/utils/user/CurrentUserResolver.java
@@ -6,23 +6,21 @@ import com.github.ezequielarroyo.postservice.dtos.input.UserDTO;
 import com.github.ezequielarroyo.postservice.entities.User;
 import com.github.ezequielarroyo.postservice.repositories.IUserRepository;
 import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class CurrentUserResolver implements ICurrentUserResolver {
     private final IUserRepository userRepository;
     private final IAuthService authService;
     private final IUserClient userClient;
     private final UserMapper userMapper;
 
-    public CurrentUserResolver(IUserRepository userRepository, IAuthService authService, IUserClient userClient, UserMapper userMapper) {
-        this.userRepository = userRepository;
-        this.authService = authService;
-        this.userClient = userClient;
-        this.userMapper = userMapper;
-    }
     public User getCurrentUser() {
         UUID currentUserID = authService.getCurrentUserId();
         return getOrFetchUser(currentUserID);
@@ -33,6 +31,7 @@ public class CurrentUserResolver implements ICurrentUserResolver {
                 .orElseGet(() -> {
                     UserDTO dto = userClient.getUserById(currentUserId);
                     if (dto == null) {
+                        log.error("User not found with ID: {}", currentUserId);
                         throw new EntityNotFoundException("User not found");
                     }
                     return userRepository.save(userMapper.toEntity(dto));
